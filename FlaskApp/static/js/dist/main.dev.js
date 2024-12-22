fc7244b2fa4934591ad77171228c16404222530e
EricHanJf <jf08942378268@gmail.com>
2024-12-22 23:49:34 +0000

    updated the js file

--- FlaskApp/static/js/dist/main.dev.js
+++ FlaskApp/static/js/dist/main.dev.js
@@ -1,165 +1,227 @@
 "use strict";
 
-// // let aliveSecond = 0;
-// // let hearBeatRate = 5000;
-// let pubnub;
-// let app_Channel = "Hydrabloom"
-// // function time() {
-// //     let d = new Date();
-// //     let currentSecond = d.getTime();
-// //     if (currentSecond - aliveSecond > hearBeatRate + 1000) {
-// //         document.getElementById("connection_id").innerHTML = "Dead";
-// //     } else {
-// //         document.getElementById("connection_id").innerHTML = "Alive";
-// //     }
-// //     setTimeout('time()', 1000);
-// // }
-// // function keepAlive() {
-// //     fetch('/keep_alive')
-// //         .then(response => {
-// //             if (response.ok) {
-// //                 let date = new Date();
-// //                 aliveSecond = date.getTime();
-// //                 return response.json();
-// //             }
-// //             throw new Error("Server offline");
-// //         })
-// //         // .then(responseJson => {
-// //         //     if (responseJson.motion == 1) {
-// //         //         document.getElementById("motion_id").innerHTML = "Motion";
-// //         //     } else {
-// //         //         document.getElementById("motion_id").innerHTML = "No Motion";
-// //         //     }
-// //         // })
-// //         .catch(error => console.log(error));
-// //     setTimeout('keepAlive()', hearBeatRate);
-// // }
-// function handleClick(cb) {
-//     if (cb.checked) {
-//         value = "on";
-//     } else {
-//         value = "off";
-//     }
-//     // sendEvent(cb.id + "-" + value);
-//     publicMessage({ "buzzer": value })
-// }
-// const setupPubNUb = () => {
-//     pubnub = new pubnub({
-//         publishkey: 'pub-c-75d6d309-722b-419e-b589-e4f425b00fcb',
-//         subscribekey: 'sub-c-ccf0e37c-5f9b-4d4e-8455-af8f49720443',
-//         userId: "Hydrabloom",
-//     })
-//     //creat a channel
-//     const channer = pubnub.channel(app_Channel);
-//     //create a subscription
-//     const subscription = channel.subscription();
-//     pubnub.addListener({
-//         status: (s) => {
-//             console.log('Status', s.category);
-//         },
-//     })
-//     subscription.onMessage = (messageEvent) => {
-//         handleMessage(messageEvent.message);
-//     }
-//     subscription.subscribe();
-// };
-// const publishMessage = async (message) => {
-//     const publishPayload = {
-//         channel: appChannel,
-//         message: message,
-//     };
-//     await pubnub.publish(publishPayload)
-// }
-// function handleMessage(message) {
-//     if (message == '"Motion":"Yes"') {
-//         document.getElementById("motion_id").innerHTML = "Yes";
-//     }
-//     if (message == '"Motion":"No"') {
-//         document.getElementById("motion_id").innerHTML = "No";
-//     }
-// }
 document.addEventListener("DOMContentLoaded", function () {
   var ledOnButton = document.getElementById("ledOnButton");
   var ledOffButton = document.getElementById("ledOffButton");
-  var statusElement = document.getElementById("status"); // Setup PubNub
+  var pumpOnButton = document.getElementById("pumpOnButton");
+  var pumpOffButton = document.getElementById("pumpOffButton");
+  var statusElement = document.getElementById("status"); // LED status element
 
+  var pumpStatusElement = document.getElementById("pumpStatus"); // Pump status element
+
+  var pumpDurationInput = document.getElementById("pumpDurationInput");
   var pubnub;
+  var channelName = "pi-hardware-channel"; // Unified PubNub setup function
 
   var setupPubNub = function setupPubNub() {
-    // Initialize PubNub
-    var pubnubConfig = {
-      publishKey: 'pub-c-75d6d309-722b-419e-b589-e4f425b00fcb',
-      subscribeKey: 'sub-c-ccf0e37c-5f9b-4d4e-8455-af8f49720443',
-      uuid: 'Hydrabloom' // Unique identifier for the client
-
-    };
-    pubnub = new PubNub(pubnubConfig); // Subscribe to the channel to listen for incoming messages
+    pubnub = new PubNub({
+      publishKey: "pub-c-5e5be15e-f582-40f2-8c1a-ca9360199f9d",
+      subscribeKey: "sub-c-a2447194-c69b-4dbe-a5e8-a4ed24799246",
+      authKey: "p0F2AkF0GmdeIWBDdHRsGQ4QQ3Jlc6VEY2hhbqFzcGktaGFyZHdhcmUtY2hhbm5lbBjPQ2dycKBDc3BjoEN1c3KgRHV1aWShbnJhc3BiZXJyeXBpNDAwGM9DcGF0pURjaGFuoENncnCgQ3NwY6BDdXNyoER1dWlkoERtZXRhoENzaWdYIPITFuBlkwRBb8BHaX4MAsO_hEak9hV4t2R_c69zO6E_",
+      // Add your auth key here
+      cryptoModule: PubNub.CryptoModule.aesCbcCryptoModule({
+        cipherKey: "secret-123"
+      }),
+      uuid: "Hydrabloom"
+    }); // Subscribe to the channel
 
     pubnub.subscribe({
-      channels: ['Hydrabloom'],
-      withPresence: true
-    }); // Set up listener for incoming messages
+      channels: [channelName]
+    }); // Set up listeners
 
     pubnub.addListener({
-      message: handleMessage,
+      message: handlePubNubMessage,
       status: function status(statusEvent) {
         console.log("PubNub status:", statusEvent);
       }
     });
-  }; // Handle the click event to publish messages to the PubNub channel
+  }; // General message handler
 
 
-  var handleClick = function handleClick(action) {
-    var message = {
-      command: action === "on" ? "LED_ON" : "LED_OFF"
-    };
-    publishMessage(message);
-  }; // Publish message to the PubNub channel
+  var handlePubNubMessage = function handlePubNubMessage(event) {
+    var channel = event.channel;
+    var message = event.message;
 
-
-  var publishMessage = function publishMessage(message) {
-    pubnub.publish({
-      channel: 'Hydrabloom',
-      // Replace with your channel
-      message: message,
-      callback: function callback(response) {
-        console.log("Message sent:", response); // Optionally, update the UI based on the response
-
-        updateStatus("LED is ".concat(message.led.toUpperCase()));
-      },
-      error: function error(_error) {
-        console.error("Error publishing message:", _error);
-      }
-    });
-  }; // Handle messages received from PubNub
-
+    if (channel === channelName) {
+      handleMessage(message); // Unified message handler
+    } else {
+      console.warn("Received message from unknown channel:", channel);
+    }
+  };
 
   var handleMessage = function handleMessage(message) {
-    console.log("Received message:", message);
-    var command = message.message.command; // Update the status UI based on the action (ON/OFF)
+    console.log("Received Message:", message);
 
-    if (command === 'LED_ON') {
-      updateStatus("LED is ON");
-    } else if (command === 'LED_OFF') {
-      updateStatus("LED is OFF");
-    } else {
-      console.log("Unknown command:", command);
+    if (message.command) {
+      if (message.command === "LED_ON") {
+        updateLEDStatus("LED is ON");
+      } else if (message.command === "LED_OFF") {
+        updateLEDStatus("LED is OFF");
+      } else if (message.command === "PUMP_ON") {
+        updatePumpStatus("Pump is ON");
+      } else if (message.command === "PUMP_OFF") {
+        updatePumpStatus("Pump is OFF");
+      } else {
+        console.warn("Unknown command:", message.command);
+      }
+    } // Handle other sensor data as before
+
+
+    if (message.temperature !== undefined && message.humidity !== undefined) {
+      document.getElementById("temperature_id").textContent = "".concat(message.temperature.toFixed(1), "\xB0C");
+      document.getElementById("humidity_id").textContent = "".concat(message.humidity.toFixed(1), "%");
+      storeDHT22Data(message.temperature, message.humidity);
     }
-  }; // Update the UI with the current LED status
 
+    if (message.lux !== undefined) {
+      document.getElementById("lux_id").textContent = "".concat(message.lux.toFixed(1), " lux");
+      storeTSL2561Data(message.lux);
+    }
 
-  var updateStatus = function updateStatus(status) {
+    if (message.soil_moisture !== undefined) {
+      document.getElementById("soil_moisture_id").textContent = "".concat(message.soil_moisture.toFixed(1), "%");
+      storeSoilMoistureData(message.soil_moisture);
+    }
+  };
+
+  var handleClick = function handleClick(action, device) {
+    var duration = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+    var message = {
+      command: "".concat(device, "_").concat(action.toUpperCase())
+    }; // Add duration if specified and action is "on"
+
+    if (action === "on" && duration) {
+      message.duration = duration;
+    }
+
+    console.log("Publishing message:", message);
+    pubnub.publish({
+      channel: channelName,
+      message: message
+    });
+  };
+
+  var updateLEDStatus = function updateLEDStatus(status) {
     statusElement.textContent = "Status: ".concat(status);
-  }; // Initialize PubNub when the page is loaded
+  }; // Function to update Pump status
 
 
-  setupPubNub(); // Event listener for the "Turn ON LED" button
+  var updatePumpStatus = function updatePumpStatus(status) {
+    pumpStatusElement.textContent = "Status: ".concat(status);
+  };
+
+  var storeDHT22Data = function storeDHT22Data(temperature, humidity) {
+    fetch("https://www.hydrabloom.online/api/store_dht22_data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        temperature: temperature,
+        humidity: humidity
+      })
+    }).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      return console.log("DHT22 data stored:", data);
+    })["catch"](function (error) {
+      return console.error("Error storing DHT22 data:", error);
+    });
+  };
+
+  var storeTSL2561Data = function storeTSL2561Data(lux) {
+    fetch("https://www.hydrabloom.online/api/store_tsl2561_data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        lux: lux
+      })
+    }).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      return console.log("TSL2561 data stored:", data);
+    })["catch"](function (error) {
+      return console.error("Error storing TSL2561 data:", error);
+    });
+  };
+
+  var storeSoilMoistureData = function storeSoilMoistureData(soilMoisture) {
+    fetch("https://www.hydrabloom.online/api/store_soil_moisture_data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        soilMoisture: soilMoisture
+      })
+    }).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      return console.log("Soil moisture data stored:", data);
+    })["catch"](function (error) {
+      return console.error("Error storing soil moisture data:", error);
+    });
+  }; // Initialize PubNub
+
+
+  setupPubNub(); // Add event listeners for LED control buttons
 
   ledOnButton.addEventListener("click", function () {
-    handleClick("on");
-  }); // Event listener for the "Turn OFF LED" button
-
+    return handleClick("on", "LED");
+  });
   ledOffButton.addEventListener("click", function () {
-    handleClick("off");
+    return handleClick("off", "LED");
+  }); // Add event listeners for Pump control buttons
+
+  pumpOnButton.addEventListener("click", function () {
+    var duration = parseInt(pumpDurationInput.value, 10);
+
+    if (duration && duration > 0) {
+      handleClick("on", "PUMP", duration);
+    } else {
+      alert("Please enter a valid duration in seconds.");
+    }
+  });
+  pumpOffButton.addEventListener("click", function () {
+    handleClick("off", "PUMP");
   });
 });
+
+function updateTimeAndDayNight() {
+  // Get the current time
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes().toString().padStart(2, '0'); // Ensure two-digit format
+
+  var seconds = now.getSeconds().toString().padStart(2, '0'); // Ensure two-digit format
+  // Update the real-time clock
+
+  document.getElementById("currentTime").textContent = "".concat(hours, ":").concat(minutes, ":").concat(seconds); // Determine the time of day
+
+  var dayOrNight;
+
+  if (hours >= 0 && hours < 6) {
+    dayOrNight = "Mid Night";
+  } else if (hours >= 6 && hours < 12) {
+    dayOrNight = "Morning";
+  } else if (hours >= 12 && hours < 18) {
+    dayOrNight = "Noon";
+  } else {
+    dayOrNight = "Evening";
+  } // Update the dayOrNight text
+
+
+  document.getElementById("dayOrNight").textContent = dayOrNight; // Repeat every second
+
+  setTimeout(updateTimeAndDayNight, 1000);
+}
+
+function toggleMenu() {
+  var mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.toggle("open"); // Optional: Toggle hamburger icon animation
+
+  var hamburger = document.querySelector(".hamburger");
+  hamburger.classList.toggle("active");
+}
--- FlaskApp/static/js/main.js
+++ FlaskApp/static/js/main.js
@@ -1,183 +1,213 @@
-// // let aliveSecond = 0;
-// // let hearBeatRate = 5000;
-// let pubnub;
-// let app_Channel = "Hydrabloom"
-
-
-// // function time() {
-// //     let d = new Date();
-// //     let currentSecond = d.getTime();
-// //     if (currentSecond - aliveSecond > hearBeatRate + 1000) {
-// //         document.getElementById("connection_id").innerHTML = "Dead";
-// //     } else {
-// //         document.getElementById("connection_id").innerHTML = "Alive";
-// //     }
-// //     setTimeout('time()', 1000);
-// // }
-// // function keepAlive() {
-// //     fetch('/keep_alive')
-// //         .then(response => {
-// //             if (response.ok) {
-// //                 let date = new Date();
-// //                 aliveSecond = date.getTime();
-// //                 return response.json();
-// //             }
-// //             throw new Error("Server offline");
-// //         })
-// //         // .then(responseJson => {
-// //         //     if (responseJson.motion == 1) {
-// //         //         document.getElementById("motion_id").innerHTML = "Motion";
-// //         //     } else {
-// //         //         document.getElementById("motion_id").innerHTML = "No Motion";
-// //         //     }
-// //         // })
-// //         .catch(error => console.log(error));
-// //     setTimeout('keepAlive()', hearBeatRate);
-
-// // }
-// function handleClick(cb) {
-//     if (cb.checked) {
-//         value = "on";
-//     } else {
-//         value = "off";
-//     }
-//     // sendEvent(cb.id + "-" + value);
-//     publicMessage({ "buzzer": value })
-// }
-
-
-// const setupPubNUb = () => {
-//     pubnub = new pubnub({
-//         publishkey: 'pub-c-75d6d309-722b-419e-b589-e4f425b00fcb',
-//         subscribekey: 'sub-c-ccf0e37c-5f9b-4d4e-8455-af8f49720443',
-//         userId: "Hydrabloom",
-//     })
-//     //creat a channel
-//     const channer = pubnub.channel(app_Channel);
-//     //create a subscription
-//     const subscription = channel.subscription();
-
-//     pubnub.addListener({
-//         status: (s) => {
-//             console.log('Status', s.category);
-//         },
-//     })
-
-//     subscription.onMessage = (messageEvent) => {
-//         handleMessage(messageEvent.message);
-
-//     }
-//     subscription.subscribe();
-
-// };
-
-// const publishMessage = async (message) => {
-//     const publishPayload = {
-//         channel: appChannel,
-//         message: message,
-//     };
-//     await pubnub.publish(publishPayload)
-// }
-
-// function handleMessage(message) {
-//     if (message == '"Motion":"Yes"') {
-//         document.getElementById("motion_id").innerHTML = "Yes";
-
-//     }
-//     if (message == '"Motion":"No"') {
-//         document.getElementById("motion_id").innerHTML = "No";
-
-//     }
-// }
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const ledOnButton = document.getElementById("ledOnButton");
     const ledOffButton = document.getElementById("ledOffButton");
-    const statusElement = document.getElementById("status");
-
-    // Setup PubNub
+    const pumpOnButton = document.getElementById("pumpOnButton");
+    const pumpOffButton = document.getElementById("pumpOffButton");
+    const statusElement = document.getElementById("status"); // LED status element
+    const pumpStatusElement = document.getElementById("pumpStatus"); // Pump status element
+    const pumpDurationInput = document.getElementById("pumpDurationInput");
+  
     let pubnub;
-
+    const channelName = "pi-hardware-channel";
+  
+    // Unified PubNub setup function
     const setupPubNub = () => {
-        // Initialize PubNub
-        const pubnubConfig = {
-            publishKey: 'pub-c-75d6d309-722b-419e-b589-e4f425b00fcb',
-            subscribeKey: 'sub-c-ccf0e37c-5f9b-4d4e-8455-af8f49720443',
-            uuid: 'Hydrabloom'  // Unique identifier for the client
-        };
-
-        pubnub = new PubNub(pubnubConfig);
-
-        // Subscribe to the channel to listen for incoming messages
-        pubnub.subscribe({
-            channels: ['Hydrabloom'],
-            withPresence: true
-        });
-
-        // Set up listener for incoming messages
-        pubnub.addListener({
-            message: handleMessage,
-            status: function (statusEvent) {
-                console.log("PubNub status:", statusEvent);
-            }
-        });
+      pubnub = new PubNub({
+        publishKey: "pub-c-5e5be15e-f582-40f2-8c1a-ca9360199f9d",
+        subscribeKey: "sub-c-a2447194-c69b-4dbe-a5e8-a4ed24799246",
+        authKey:
+          "p0F2AkF0GmdeIWBDdHRsGQ4QQ3Jlc6VEY2hhbqFzcGktaGFyZHdhcmUtY2hhbm5lbBjPQ2dycKBDc3BjoEN1c3KgRHV1aWShbnJhc3BiZXJyeXBpNDAwGM9DcGF0pURjaGFuoENncnCgQ3NwY6BDdXNyoER1dWlkoERtZXRhoENzaWdYIPITFuBlkwRBb8BHaX4MAsO_hEak9hV4t2R_c69zO6E_", // Add your auth key here
+        cryptoModule: PubNub.CryptoModule.aesCbcCryptoModule({
+          cipherKey: "secret-123",
+        }),
+        uuid: "Hydrabloom",
+      });
+  
+      // Subscribe to the channel
+      pubnub.subscribe({
+        channels: [channelName],
+      });
+  
+      // Set up listeners
+      pubnub.addListener({
+        message: handlePubNubMessage,
+        status: (statusEvent) => {
+          console.log("PubNub status:", statusEvent);
+        },
+      });
     };
-
-    // Handle the click event to publish messages to the PubNub channel
-    const handleClick = (action) => {
-        const message = {
-            command: action === "on" ? "LED_ON" : "LED_OFF"
-        };
-        publishMessage(message);
+  
+    // General message handler
+    const handlePubNubMessage = (event) => {
+      const channel = event.channel;
+      const message = event.message;
+  
+      if (channel === channelName) {
+        handleMessage(message); // Unified message handler
+      } else {
+        console.warn("Received message from unknown channel:", channel);
+      }
     };
-
-    // Publish message to the PubNub channel
-    const publishMessage = (message) => {
-        pubnub.publish({
-            channel: 'Hydrabloom',  // Replace with your channel
-            message: message,
-            callback: function (response) {
-                console.log("Message sent:", response);
-                // Optionally, update the UI based on the response
-                updateStatus(`LED is ${message.led.toUpperCase()}`);
-            },
-            error: function (error) {
-                console.error("Error publishing message:", error);
-            }
-        });
-    };
-
-    // Handle messages received from PubNub
+  
     const handleMessage = (message) => {
-        console.log("Received message:", message);
-        const command = message.message.command;
-
-        // Update the status UI based on the action (ON/OFF)
-        if (command === 'LED_ON') {
-            updateStatus("LED is ON");
-        } else if (command === 'LED_OFF') {
-            updateStatus("LED is OFF");
+      console.log("Received Message:", message);
+  
+      if (message.command) {
+        if (message.command === "LED_ON") {
+          updateLEDStatus("LED is ON");
+        } else if (message.command === "LED_OFF") {
+          updateLEDStatus("LED is OFF");
+        } else if (message.command === "PUMP_ON") {
+          updatePumpStatus("Pump is ON");
+        } else if (message.command === "PUMP_OFF") {
+          updatePumpStatus("Pump is OFF");
         } else {
-            console.log("Unknown command:", command);
+          console.warn("Unknown command:", message.command);
         }
+      }
+  
+      // Handle other sensor data as before
+      if (message.temperature !== undefined && message.humidity !== undefined) {
+        document.getElementById(
+          "temperature_id"
+        ).textContent = `${message.temperature.toFixed(1)}°C`;
+        document.getElementById(
+          "humidity_id"
+        ).textContent = `${message.humidity.toFixed(1)}%`;
+  
+        storeDHT22Data(message.temperature, message.humidity);
+      }
+  
+      if (message.lux !== undefined) {
+        document.getElementById("lux_id").textContent = `${message.lux.toFixed(
+          1
+        )} lux`;
+        storeTSL2561Data(message.lux);
+      }
+  
+      if (message.soil_moisture !== undefined) {
+        document.getElementById(
+          "soil_moisture_id"
+        ).textContent = `${message.soil_moisture.toFixed(1)}%`;
+        storeSoilMoistureData(message.soil_moisture);
+      }
     };
-
-    // Update the UI with the current LED status
-    const updateStatus = (status) => {
-        statusElement.textContent = `Status: ${status}`;
+  
+    const handleClick = (action, device, duration = null) => {
+      const message = { command: `${device}_${action.toUpperCase()}` };
+  
+      // Add duration if specified and action is "on"
+      if (action === "on" && duration) {
+        message.duration = duration;
+      }
+  
+      console.log("Publishing message:", message);
+      pubnub.publish({
+        channel: channelName,
+        message: message,
+      });
     };
-
-    // Initialize PubNub when the page is loaded
+  
+    const updateLEDStatus = (status) => {
+      statusElement.textContent = `Status: ${status}`;
+    };
+  
+    // Function to update Pump status
+    const updatePumpStatus = (status) => {
+      pumpStatusElement.textContent = `Status: ${status}`;
+    };
+  
+    const storeDHT22Data = (temperature, humidity) => {
+      fetch("https://www.hydrabloom.online/api/store_dht22_data", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ temperature, humidity }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log("DHT22 data stored:", data))
+        .catch((error) => console.error("Error storing DHT22 data:", error));
+    };
+  
+    const storeTSL2561Data = (lux) => {
+      fetch("https://www.hydrabloom.online/api/store_tsl2561_data", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ lux }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log("TSL2561 data stored:", data))
+        .catch((error) => console.error("Error storing TSL2561 data:", error));
+    };
+  
+    const storeSoilMoistureData = (soilMoisture) => {
+      fetch("https://www.hydrabloom.online/api/store_soil_moisture_data", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ soilMoisture }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log("Soil moisture data stored:", data))
+        .catch((error) =>
+          console.error("Error storing soil moisture data:", error)
+        );
+    };
+  
+    // Initialize PubNub
     setupPubNub();
-
-    // Event listener for the "Turn ON LED" button
-    ledOnButton.addEventListener("click", () => {
-        handleClick("on");
+  
+    // Add event listeners for LED control buttons
+    ledOnButton.addEventListener("click", () => handleClick("on", "LED"));
+    ledOffButton.addEventListener("click", () => handleClick("off", "LED"));
+  
+    // Add event listeners for Pump control buttons
+    pumpOnButton.addEventListener("click", () => {
+      const duration = parseInt(pumpDurationInput.value, 10);
+      if (duration && duration > 0) {
+        handleClick("on", "PUMP", duration);
+      } else {
+        alert("Please enter a valid duration in seconds.");
+      }
     });
-
-    // Event listener for the "Turn OFF LED" button
-    ledOffButton.addEventListener("click", () => {
-        handleClick("off");
+  
+    pumpOffButton.addEventListener("click", () => {
+      handleClick("off", "PUMP");
     });
-});
+  });
+  
+  function updateTimeAndDayNight() {
+    // Get the current time
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0'); // Ensure two-digit format
+    const seconds = now.getSeconds().toString().padStart(2, '0'); // Ensure two-digit format
+    
+    // Update the real-time clock
+    document.getElementById("currentTime").textContent = `${hours}:${minutes}:${seconds}`;
+  
+    // Determine the time of day
+    let dayOrNight;
+    if (hours >= 0 && hours < 6) {
+        dayOrNight = "Mid Night";
+    } else if (hours >= 6 && hours < 12) {
+        dayOrNight = "Morning";
+    } else if (hours >= 12 && hours < 18) {
+        dayOrNight = "Noon";
+    } else {
+        dayOrNight = "Evening";
+    }
+  
+    // Update the dayOrNight text
+    document.getElementById("dayOrNight").textContent = dayOrNight;
+  
+    // Repeat every second
+    setTimeout(updateTimeAndDayNight, 1000);
+  }
+  
+  function toggleMenu() {
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.classList.toggle("open");
+  
+    // Optional: Toggle hamburger icon animation
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.classList.toggle("active");
+  }
